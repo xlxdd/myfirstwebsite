afdfa958bfc6bc0f215d2766f158ad17cb61ad70
xlxdd <393025885@qq.com>
2023-06-08 21:43:30 +0800

    优化文件结构，完成管理员主页

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # static_url_path=,static_folder=,template_folder=
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def login():  # put application's code here
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
